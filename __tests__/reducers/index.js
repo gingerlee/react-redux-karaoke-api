b91dc6a055a3f1d76af8a0d7d876f047d4a05d7a
Cheryl Frankenfied and Ginger Lee Kretschmer <cheryl@>
2018-02-26 10:25:13 -0800

    Add action creators to project

--- __tests__/reducers/index.js
+++ __tests__/reducers/index.js
@@ -1,8 +1,9 @@
-import constants from "./../../src/constants";
+import constants from './../../src/constants';
 import songChangeReducer from './../../src/reducers/songChangeReducer';
 import lyricChangeReducer from './../../src/reducers/lyricChangeReducer';
 import rootReducer from './../../src/reducers/';
 import { createStore } from 'redux';
+import * as actions from './../../src/actions';
 
 describe('Karaoke App', () => {
   const { initialState, types } = constants;
@@ -10,15 +11,15 @@ describe('Karaoke App', () => {
 
   describe('lyricChangeReducer', () => {
     it('Should accept and return initial state.', () => {
-      expect(lyricChangeReducer(initialState.songsById, { type: null })).toEqual(initialState.songsById);
+      expect(lyricChangeReducer(initialState, { type: null })).toEqual(initialState);
     });
 
     it('Should update currently-displayed lyric of song', () => {
-      expect(lyricChangeReducer(initialState.songsById, { type: 'NEXT_LYRIC', currentSongId: 2 })[2].arrayPosition).toEqual(initialState.songsById[2].arrayPosition + 1);
+      expect(lyricChangeReducer(initialState.songsById, actions.nextLyric(2))[2].arrayPosition).toEqual(initialState.songsById[2].arrayPosition + 1);
     });
 
     it('Should restart song', () => {
-      expect(lyricChangeReducer(initialState.songsById, { type: 'RESTART_SONG', currentSongId: 1 })[1].arrayPosition).toEqual(0);
+      expect(lyricChangeReducer(initialState.songsById, actions.restartSong(1))[1].arrayPosition).toEqual(0);
     });
   });
 
@@ -28,7 +29,7 @@ describe('Karaoke App', () => {
     });
 
     it('Should change selectedSong.', () => {
-      expect(songChangeReducer(initialState, { type: 'CHANGE_SONG', newSelectedSongId: 1 })).toEqual(1);
+      expect(songChangeReducer(initialState, actions.changeSong(1))).toEqual(1);
     });
   });
 
